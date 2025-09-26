fd5d3c8a122aefbeb8d5725eeb71f2886bbd14cf
edwardlab <56959323+edwardcodelab@users.noreply.github.com>
2025-09-26 19:08:26 +0800

    Add files via upload

---
+++ conf/metadata.php
@@ -0,0 +1,3 @@
+<?php
+$meta['pages'] = array('multiline');  // Existing
+$meta['position'] = array('multichoice', '_choices' => array('top' => 'After Heading (prepend)', 'bottom' => 'End of Page (append)'));
